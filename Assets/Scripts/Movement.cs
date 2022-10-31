b5cdc4354f6f9079ea396542605c6dbf8520352b
Aman Kumar Matta <amankumarmatta0@gmail.com>
2022-10-31 14:24:34 +0530

    Bleh

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -33,7 +33,7 @@ public class Movement : MonoBehaviour
         {
             transform.localScale = Vector3.one;
         }
-        else if(horInput < -0.01f)
+        else if (horInput < -0.01f)
         {
             transform.localScale = new Vector3(-1, 1, 1);
         }
