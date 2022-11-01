c9e2f20fd0f87eacd12821c652e3ccc3f60897d1
Aman Kumar Matta <amankumarmatta0@gmail.com>
2022-11-01 12:35:20 +0530

    Implemented Shooting

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -2,6 +2,10 @@
 
 public class Attack : MonoBehaviour
 {
+    [SerializeField]private float attackCooldown;
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private GameObject[] fireballs;
+    private float cooldownTimer = Mathf.Infinity;
     private Animator anim;
     private Movement movement;
 
@@ -9,5 +13,37 @@ public class Attack : MonoBehaviour
     {
         anim = GetComponent<Animator>();
         movement = GetComponent<Movement>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButton(0) && cooldownTimer > attackCooldown && movement.Attack())
+        {
+            AttackMode();
+
+        }
+        cooldownTimer += Time.deltaTime;
+    }
+
+    private void AttackMode()
+    {
+        anim.SetTrigger("Attack");
+        cooldownTimer = 0;
+
+        //Object Pooling
+        fireballs[FindFireBall()].transform.position = firePoint.position;
+        fireballs[FindFireBall()].GetComponent<Fireball>().SetDirection(Mathf.Sign(transform.localScale.x));
+    }
+
+    private int FindFireBall()
+    {
+        for (int i = 0; i < fireballs.Length; i++)
+        {
+            if (fireballs[i].activeInHierarchy)
+            {
+                return i;
+            }
+        }
+        return 0;
     }
 }
