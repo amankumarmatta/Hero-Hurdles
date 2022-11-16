056a4bf62341d395d3b1e9a19503be2f2267b918
Aman Kumar Matta <amankumarmatta0@gmail.com>
2022-11-16 19:19:23 +0530

    Minor Changes to environment

--- Assets/Scripts/Enemies/FireTrap.cs
+++ Assets/Scripts/Enemies/FireTrap.cs
@@ -26,10 +26,14 @@ public class FireTrap : MonoBehaviour
         if (collision.tag == "Player")
         {
             if (!triggered)
+            {
                 StartCoroutine(ActivateFiretrap());
+            }
 
             if (active)
+            {
                 collision.GetComponent<Health>().TakeDamage(damage);
+            }
         }
     }
     private IEnumerator ActivateFiretrap()
@@ -51,49 +55,3 @@ public class FireTrap : MonoBehaviour
         anim.SetBool("Activated", false);
     }
 }
-    #region Old Code
-    /*[SerializeField] private float damage;
-
-    [Header("Firetrap Timers")]
-    [SerializeField] private float activateDelay;
-    [SerializeField] private float activeTime;
-
-    private Animator anim;
-    private SpriteRenderer spriteRenderer;
-
-    private bool triggered;
-    private bool active;
-
-    private void Awake()
-    {
-        anim = GetComponent<Animator>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.tag == "Player")
-        {
-            if (!triggered)
-            {
-                StartCoroutine(ActivateFireTrap());
-            }
-            if (active)
-            {
-                collision.GetComponent<Health>().TakeDamage(damage);
-            }
-        }
-    }
-
-    private IEnumerator ActivateFireTrap()
-    {
-        triggered = true;
-        spriteRenderer.color = Color.red;
-        yield return new WaitForSeconds(activateDelay);
-        spriteRenderer.color = Color.white;
-        active = true;
-        yield return new WaitForSeconds(activeTime);
-        active = false;
-        triggered = false;
-    }*/
-    #endregion
