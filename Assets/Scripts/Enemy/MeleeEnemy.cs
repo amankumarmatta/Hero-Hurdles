0f6623f44fd7471e690cc920df1c5facbe18318d
Aman Kumar Matta <amankumarmatta0@gmail.com>
2023-03-25 09:02:47 +0530

    Minor Fixes to code

--- Assets/Scripts/Enemy/MeleeEnemy.cs
+++ Assets/Scripts/Enemy/MeleeEnemy.cs
@@ -36,7 +36,7 @@ public class MeleeEnemy : MonoBehaviour
             if (cooldownTimer >= attackCooldown)
             {
                 cooldownTimer = 0;
-                anim.SetTrigger("Melee");
+                anim.SetTrigger("Attack");
             }
         }
 
