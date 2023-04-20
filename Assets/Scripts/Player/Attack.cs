dc09bca838b0d930174531cde6a06640787eef30
Aman Kumar Matta <amankumarmatta0@gmail.com>
2023-04-20 12:57:44 +0530

    Updated Player

--- Assets/Scripts/Player/Attack.cs
+++ Assets/Scripts/Player/Attack.cs
@@ -3,10 +3,7 @@
 public class Attack : MonoBehaviour
 {
     [SerializeField]private float attackCooldown;
-    [SerializeField] private Transform firePoint;
-    [SerializeField] private GameObject[] fireballs;
     private PlayerControls controls;
-    private float cooldownTimer = Mathf.Infinity;
     private Animator anim;
     private bool isGrounded;
 
@@ -20,34 +17,14 @@ public class Attack : MonoBehaviour
         controls.Move.Shoot.performed += ctx => AttackMode();     
     }
 
-    private void Update()
-    {
-        cooldownTimer += Time.deltaTime;
-    }
-
     private void AttackMode()
     {
         if (isGrounded == true)
         {
-            anim.SetTrigger("Attack");
-            cooldownTimer = 0;
-            
-            fireballs[FindFireBall()].transform.position = firePoint.position;
-            fireballs[FindFireBall()].GetComponent<Fireball>().SetDirection(Mathf.Sign(transform.localScale.x));
+            anim.SetTrigger("Attack");               
         }
     }
 
-    private int FindFireBall()
-    {
-        for (int i = 0; i < fireballs.Length; i++)
-        {
-            if (!fireballs[i].activeInHierarchy)
-            {
-                return i;
-            }
-        }
-        return 0;
-    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Ground")
